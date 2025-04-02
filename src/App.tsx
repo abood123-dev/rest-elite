37f10fce21640123187899a61b697fabd85351d1
Abood <abood@gmail.com>
2025-04-02 06:03:22 -0700

    trying hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import react from 'react';
-import './App.css';
-import React from 'react';
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 function App() {
   return (
-    <div className="App">
-    hello
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { createBrowserRouter } from 'react-router-dom';
-import { RouterProvider } from 'react-router-dom';
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element:<App/> 
-  },
-]);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <>  
-      <RouterProvider router={router} />
-  </>
-  );
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
